974a7ac468b9f2e51b8b4ed585367f23e5fdd8a6
sam <sam@freighttrust.com>
2020-11-29 17:13:07 -0800

    feat(release): 1.0.2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const latex = require('node-latex')
-const fs = require('fs')
+const latex = require("node-latex");
+const fs = require("fs");
 // TODO: Fix so that npm can generate the pdf
 const { createReadStream, createWriteStream } = require("fs");
 const { join } = require("path");
@@ -9,7 +9,7 @@ const input = createReadStream(join(__dirname, "latex/*.tex"));
 const output = createWriteStream(join(__dirname, "output.pdf"));
 
 latex(input).pipe(output);
- 
-pdf.pipe(output)
-pdf.on('error', err => console.error(err))
-pdf.on('finish', () => console.log('PDF generated!'))
+
+pdf.pipe(output);
+pdf.on("error", (err) => console.error(err));
+pdf.on("finish", () => console.log("PDF generated!"));
